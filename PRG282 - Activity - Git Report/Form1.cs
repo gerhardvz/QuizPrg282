63001c8dd1de9cd472e0336c0958b234e51aa461
Gerhard van Zyl <geruharuto1@gmail.com>
2021-10-04 10:06:48 +0200

    Added ReadFile Functionality
    Created ReadFile Method and implemented it on button click to populate listBox1

--- PRG282 - Activity - Git Report/Form1.cs
+++ PRG282 - Activity - Git Report/Form1.cs
@@ -19,6 +19,34 @@ namespace PRG282___Activity___Git_Report
 
         private void button1_Click(object sender, EventArgs e)
         {
+         try
+            {
+                List<string> users = ReadFile("UserData.txt");
+                listBox1.Items.Add(users);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+         public List<string> ReadFile(string filepath)
+        {
+            FileStream file = new FileStream(filepath, FileMode.Open);
+            StreamReader reader = new StreamReader(file);
+            List<string> users = new List<string>();
+            if (Directory.Exists(filepath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    users.Add(line);
+                }
+                reader.Close();
+                file.Close();
+                return users;
+            }
+            else
+                throw new Exception("File \"" + filepath + "\" does not Exist");
 
         }
     }
